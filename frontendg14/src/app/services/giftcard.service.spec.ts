cd4915531af916cd95991b71c180d3ddde7d3748
Jose Cabrera <x.pe1891@gmail.com>
2020-10-30 19:52:15 -0600

    iniciado pruebas para obtener giftcards

---
+++ frontendg14/src/app/services/giftcard.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { GiftcardService } from './giftcard.service';
+
+describe('GiftcardService', () => {
+  let service: GiftcardService;
+  let httpMock: HttpTestingController;
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        GiftcardService
+      ],
+    });
+    service = TestBed.inject(GiftcardService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get list of giftcards', () => {
+    let giftcards = service.getGiftcard();
+    expect(giftcards).toBeInstanceOf(Array);
+  });
+});
---
+++ frontendg14/src/app/services/giftcard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+//import { isNullOrUndefined } from 'util';
+// import {login, registro}  from '../models/user-interfaces'
+import {Router} from '@angular/router';
+@Injectable({
+  providedIn: 'root'
+})
+export class GiftcardService {
+
+  constructor(private http:HttpClient) { }
+
+  headers : HttpHeaders = new HttpHeaders({
+    "Content-Type":"application/json"
+  })
+
+  //get giftcards
+  getGiftcard(){
+
+  }
+}
